6aadfe8a5d0df25e893914e7a67086e34729083d
shakti <kumarshakti2017@gmail.com>
2025-07-20 08:05:46 +0530

    added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const {PORT} = require("./config/serverConfigs");
 const apiRoutes = require("./routes/index");
 const app = express();
 
+// const db = require("./models/index");
+
 const prepareAndStartServer = ()=>{
     app.use(express.json());
     app.use("/api",apiRoutes);
@@ -12,6 +14,10 @@ const prepareAndStartServer = ()=>{
             console.log("Error while started the auth server");
             throw error;
         }
+        // syncing the DB
+        // if(process.env.SYNC_DB){
+        //     db.sequelize.sync({alter:true});
+        // }
         console.log(`Auth server started at port: ${PORT}`);
     })
 }
